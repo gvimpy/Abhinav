e1d8fe88d7f412818ab7f4c556a028ca9b7aa6e3
gvimpy <88411747+gvimpy@users.noreply.github.com>
2021-09-18 16:40:25 -0700

    Add files via upload

---
+++ direction = input ("choose operation mul.py
@@ -0,0 +1,46 @@
+direction = input ("choose operation mult add sub div:")
+if direction == "add":
+    ans = input ("Number One:")
+    rad = input ("Number Two:")
+    result = int(ans) + int(rad)
+    print (result)
+if direction == "mult":
+    an = input ("Number One:")
+    ra = input ("Number Two:")
+    ww = int(an) * int(ra)
+    print (ww)
+if direction == "sub":
+    a = input ("Number One:")
+    r = input ("Number Two:")
+    Thang = int(a) - int(r)
+    print (Thang)
+if direction == "div":
+    abhinavscool = input ("Number One:")
+    abhinavsuck = input ("Number Two:")
+    hi = int(abhinavscool) / int(abhinavsuck)
+    print (hi)
+would = input ("Would you like to continue:")
+if would == "yes":
+    direction = input ("choose operation mult add sub div:")
+if direction == "add":
+    ans = input ("Number One:")
+    rad = input ("Number Two:")
+    result = int(ans) + int(rad)
+    print (result)
+if direction == "mult":
+    an = input ("Number One:")
+    ra = input ("Number Two:")
+    ww = int(an) * int(ra)
+    print (ww)
+if direction == "sub":
+    a = input ("Number One:")
+    r = input ("Number Two:")
+    Thang = int(a) - int(r)
+    print (Thang)
+if direction == "div":
+    abhinavscool = input ("Number One:")
+    abhinavsuck = input ("Number Two:")
+    hi = int(abhinavscool) / int(abhinavsuck)
+    print (hi)
+if would == "no":
+    print ("See you later then.")
